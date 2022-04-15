a25458a1e2bca672368dcd1ad0b4b1591ce05d89
niki571 <super571@sina.com>
2022-04-16 06:12:05 +0800

    chore: 删除信息

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2022-04-15 14:56:46
- * @LastEditTime: 2022-04-16 06:10:24
- * @LastEditors: your name
+ * @LastEditTime: 2022-04-16 06:11:32
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /bus-box-rent/src/config.ts
  */
-export const JWT_SECRET = 'paopaotang';
+export const JWT_SECRET = 'JWT_SECRET';
 
 export const APP_ID = 'APP_ID';
 
